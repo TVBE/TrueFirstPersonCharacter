a9c1f1cb33c254fe8a5490f05b985aad8d0115c7
Tim Verberne <timverbe@gmail.com>
2023-01-09 16:19:44 +0100

    updated player controller

---
+++ Source/TrueFirstPerson/Private/PlayerLocomotionEventType.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerLocomotionEventType.h"
+
+PlayerLocomotionEventType::PlayerLocomotionEventType()
+{
+}
+
+PlayerLocomotionEventType::~PlayerLocomotionEventType()
+{
+}
