77ad4510a7340de0d29af0efd9323ceb8cce5974
Tim Verberne <timverbe@gmail.com>
2023-01-21 16:37:29 +0100

    added log categories

--- Source/TrueFirstPerson/TrueFirstPerson.cpp
+++ Source/TrueFirstPerson/TrueFirstPerson.cpp
@@ -4,3 +4,10 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrueFirstPerson, "TrueFirstPerson" );
+
+DEFINE_LOG_CATEGORY(LogPlayerCharacter)
+DEFINE_LOG_CATEGORY(LogPlayerCharacterController)
+DEFINE_LOG_CATEGORY(LogPlayerCharacterConfiguration)
+DEFINE_LOG_CATEGORY(LogPlayerCameraController)
+DEFINE_LOG_CATEGORY(LogPlayerFlashlightComponent)
+DEFINE_LOG_CATEGORY(LogInteractableActor)
--- Source/TrueFirstPerson/TrueFirstPerson.h
+++ Source/TrueFirstPerson/TrueFirstPerson.h
@@ -4,3 +4,10 @@
 
 #include "CoreMinimal.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerCharacter, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerCharacterController, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerCharacterConfiguration, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerCameraController, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayerFlashlightComponent, Log, All)
+DECLARE_LOG_CATEGORY_EXTERN(LogInteractableActor, Log, All)
+
